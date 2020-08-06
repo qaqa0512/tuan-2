3667ca8481299cdb8315d2851fecd4b45bd11ec1
qaqa0512 <nguyenquocanh0512@gmail.com>
2020-08-06 11:08:45 +0700

    Tuan2_2020

---
+++ Tuan2_2020/Bai1.cpp
@@ -0,0 +1,109 @@
+#include <C:\Users\Administrator\Documents\visual studio 2015\Projects\Tuan2_2020\Tuan2_2020\Dependencies\glew\glew.h>
+#include <C:\Users\Administrator\Documents\visual studio 2015\Projects\Tuan2_2020\Tuan2_2020\Dependencies\freeglut\freeglut.h>
+
+#include <math.h>
+#define PI 3.14159265f
+
+char title[] = "Bouncing Ball (2D)";
+int windowWidth = 640;
+int windowHeight = 480;
+int windowPosX = 50;
+int windowPosy = 50;
+
+GLfloat ballRadius = 0.5f;
+GLfloat ballX = 0.0f;
+GLfloat ballY = 0.0f;
+GLfloat ballXMax, ballXMin, ballYMax, ballYMin;
+GLfloat xSpeed = 0.02f;
+GLfloat ySpeed = 0.007f;
+int refreshMillis = 30;
+
+GLdouble clipAreaXLeft, clipAreaRight, clipAreaYBottom, clipAreaYTop;
+
+void initGL() {
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(ballX, ballY, 0.0f);
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(0.0f, 0.0f, 1.0f); //Blue
+	glVertex2f(0.0f, 0.0f);
+	int numSegments = 100;
+	GLfloat angle;
+	for (int i = 0; i < numSegments; i++) {
+		angle = i * 2.0f * PI / numSegments;
+		glVertex2f(cos(angle) * ballRadius, sin(angle) * ballRadius);
+	}
+	glEnd();
+
+	glutSwapBuffers();
+
+	ballX += xSpeed;
+	ballY += ySpeed;
+
+	if (ballX > ballXMax) {
+		ballX = ballXMax;
+		xSpeed = -xSpeed;
+	}
+	else if(ballX < ballXMin){
+		ballX = ballXMin;
+		xSpeed = -xSpeed;
+	}
+	if (ballY > ballYMax) {
+		ballY = ballYMax;
+		ySpeed = -ySpeed;
+	}
+	else if (ballY < ballYMin) {
+		ballY = ballYMin;
+		ySpeed = -ySpeed;
+	}
+}
+
+void reshape(GLsizei width, GLsizei height){
+	if (height == 0) height = 1;
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (width >= height) {
+		clipAreaXLeft = -1.0 * aspect;
+		clipAreaRight  = 1.0 * aspect;
+		clipAreaYBottom = -1.0;
+		clipAreaYTop = 1.0;
+	}
+	else {
+		clipAreaXLeft = -1.0;
+		clipAreaRight = 1.0;
+		clipAreaYBottom = -1.0 / aspect;
+		clipAreaYTop = 1.0 / aspect;
+	}
+	gluOrtho2D(clipAreaXLeft, clipAreaRight, clipAreaYBottom, clipAreaYTop);
+	ballXMin = clipAreaXLeft + ballRadius;
+	ballXMax = clipAreaRight - ballRadius;
+	ballYMin = clipAreaYBottom + ballRadius;
+	ballYMax = clipAreaYTop - ballRadius;
+}
+void Timer(int value) {
+	glutPostRedisplay();
+	glutTimerFunc(refreshMillis, Timer, 0);
+}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutInitWindowPosition(windowPosX, windowPosy);
+	glutCreateWindow(title);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutTimerFunc(0, Timer, 0);
+	initGL();
+	glutMainLoop();
+	return 0;
+}
